b53a71e91d40624f1d6ceebcc7bda627a16a6db4
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-04-02 06:38:18 -0500

    feat: :sparkles: `Ejercico 2.`
    Ejercicio2.js

---
+++ Ejercicio2.js
@@ -0,0 +1,9 @@
+// 2. Crea una función que reciba un array de números y devuelva el mayor de ellos
+
+let numeros = [1,2,3,4,5]
+
+function mayor() {
+    return(Math.max(...numeros))
+}
+
+console.log(mayor(...numeros))
