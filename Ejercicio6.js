6e2f72d02fccb8b59091e9d551227b33b3538de1
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-04-02 08:20:52 -0500

    feat: :sparkles: `Ejercicio 6.`
    6. Crea una función que reciba dos arrays y devuelva un nuevo array que contenga los elementos comunes entre ambos

---
+++ Ejercicio6.js
@@ -0,0 +1,17 @@
+// 6. Crea una función que reciba dos arrays y devuelva un nuevo array que contenga los elementos comunes entre ambos
+
+let colores =  [
+    "amarrillo",
+    "rojo",
+    "azul"
+]
+let animales =  [
+    "cocodrilo",
+    "gallina",
+    "elefante"
+]
+
+function arrayJuntas() {
+    console.log(colores.concat(animales))
+}
+arrayJuntas()
