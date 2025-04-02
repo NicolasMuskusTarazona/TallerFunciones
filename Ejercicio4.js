dc1549baa9914cbc0e88c383a2734a208341000b
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-04-02 08:13:37 -0500

    feat: :sparkles: `Ejercicio 4.`
    4. Crea una función que reciba un array de strings y devuelva un nuevo array con las strings en mayúsculas

---
+++ Ejercicio4.js
@@ -0,0 +1,13 @@
+// 4. Crea una función que reciba un array de strings y devuelva un nuevo array con las strings en mayúsculas
+
+let nombres = [
+    "nicolas",
+    "danilo",
+    "darwin",
+    "danna",
+    "diego"
+]
+
+let mayusculas = nombres.map(nombresMayusuclas => nombresMayusuclas.toUpperCase())
+
+console.log(mayusculas)
