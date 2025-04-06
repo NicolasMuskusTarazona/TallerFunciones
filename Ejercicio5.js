5283816f8d35a45efcd02fe195307d7b232a98e8
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-04-06 13:26:39 -0500

    feat: :sparkles: `Numeros primos.`
    5. Crea una función que reciba un número y devuelva true si es primo, y false en caso contrario

---
+++ Ejercicio5.js
@@ -0,0 +1,5 @@
+// 5. Crea una función que reciba un número y devuelva true si es primo, y false en caso contrario
+function primo(numero) {
+    console.log((numero %2 === 0)? true : false)
+}
+primo(10)
