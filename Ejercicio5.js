04e3f5e53f88bd9fdbd8d64d5e1f1e8a22d76c91
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-04-06 21:48:44 -0500

    feat: :bug: `Encontrar numeros primos.`

--- Ejercicio5.js
+++ Ejercicio5.js
@@ -1,5 +1,19 @@
 // 5. Crea una función que reciba un número y devuelva true si es primo, y false en caso contrario
-function primo(numero) {
-    console.log((numero %2 === 0)? true : false)
+function esPrimo(numero) {
+    if (numero < 2) {
+        return false
+    }
+    for (let i = 2; i <= Math.sqrt(numero); i++) {
+        if (numero % i === 0) {
+            return false
+        }
+    }
+    return true
 }
-primo(10)
+
+let numero = Number(prompt("Ingrese un numero: "))
+if (esPrimo(numero)) {
+    console.log(numero + " es primo")
+} else {
+    console.log(numero + " no es primo")
+}
